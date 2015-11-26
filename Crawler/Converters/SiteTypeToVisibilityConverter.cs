bcf1bdd3019e6435a7812cfba316806b4a033bf7
v0v <vova.dubna@gmail.com>
2015-11-26 15:54:06 +0300

    Modify Extensions

---
+++ Crawler/Converters/SiteTypeToVisibilityConverter.cs
@@ -0,0 +1,30 @@
+﻿// This file contains my intellectual property. Release of this file requires prior approval from me.
+// 
+// Copyright (c) 2015, v0v All Rights Reserved
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+using Interfaces.Enums;
+
+namespace Crawler.Converters
+{
+    public class SiteTypeToVisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var vtype = (SiteType)value;
+            return vtype == SiteType.YouTube ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
---
+++ Extensions/EnumerableExtensions.cs
@@ -0,0 +1,24 @@
+﻿// This file contains my intellectual property. Release of this file requires prior approval from me.
+// 
+// Copyright (c) 2015, v0v All Rights Reserved
+
+using System;
+using System.Collections.Generic;
+
+namespace Extensions
+{
+    public static class EnumerableExtensions
+    {
+        #region Static Methods
+
+        public static void ForEach<TItem>(this IEnumerable<TItem> sequence, Action<TItem> action)
+        {
+            foreach (TItem obj in sequence)
+            {
+                action(obj);
+            }
+        }
+
+        #endregion
+    }
+}
