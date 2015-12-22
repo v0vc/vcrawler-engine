05af9f9b0fdcf4a9cfe767ede25cd3618f7b61f0
v0vc <ias4eg@rambler.ru>
2015-12-22 23:44:18 +0300

    Fix UI settings progressbar

---
+++ Crawler/Converters/BoolToVisibilityConverter.cs
@@ -0,0 +1,34 @@
+﻿// This file contains my intellectual property. Release of this file requires prior approval from me.
+// 
+// 
+// Copyright (c) 2015, v0v All Rights Reserved
+
+using System;
+using System.Globalization;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Crawler.Converters
+{
+    public class BoolToVisibilityConverter : IValueConverter
+    {
+        #region IValueConverter Members
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is bool))
+            {
+                return Visibility.Collapsed;
+            }
+            var objValue = (bool)value;
+            return objValue ? Visibility.Visible : Visibility.Collapsed;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
