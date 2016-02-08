bbe69ab93a561c55b09ea74098c21fcc32bb73b5
v0v <vova.dubna@gmail.com>
2016-02-08 15:17:04 +0300

    Rework state UI

--- Crawler/ViewModels/StateChannel.cs
+++ Crawler/ViewModels/StateChannel.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
 using System.ComponentModel;
+using System.IO;
 using System.Linq;
 using System.Net;
 using System.Reflection;
@@ -31,6 +32,14 @@ namespace Crawler.ViewModels
         private readonly List<IVideoItem> watchedList;
         private readonly IPlaylist watchedPlaylist;
 
+        private readonly Dictionary<string, object> supportedStates = new Dictionary<string, object>
+        {
+            { "Crawler.Images.new_48.png", SyncState.Added },
+            { "Crawler.Images.time_48.png", WatchState.Planned },
+            { "Crawler.Images.tick_48.png", WatchState.Watched },
+            { "Crawler.Images.done_48.png", ItemState.LocalYes }
+        };
+
         #endregion
 
         #region Constructors
@@ -40,13 +49,20 @@ namespace Crawler.ViewModels
             this.db = db;
             ChannelPlaylists = new ObservableCollection<IPlaylist>();
             ChannelItems = new ObservableCollection<IVideoItem>();
-            addedList = new List<IVideoItem>();
-            plannedList = new List<IVideoItem>();
-            watchedList = new List<IVideoItem>();
-            addedPlaylist = PlaylistFactory.CreatePlaylist(SiteType.NotSet);
-            plannedPlaylist = PlaylistFactory.CreatePlaylist(SiteType.NotSet);
-            watchedPlaylist = PlaylistFactory.CreatePlaylist(SiteType.NotSet);
-            InitServicePlaylists();
+            //addedList = new List<IVideoItem>();
+            //plannedList = new List<IVideoItem>();
+            //watchedList = new List<IVideoItem>();
+            //addedPlaylist = PlaylistFactory.CreatePlaylist(SiteType.NotSet);
+            //plannedPlaylist = PlaylistFactory.CreatePlaylist(SiteType.NotSet);
+            //watchedPlaylist = PlaylistFactory.CreatePlaylist(SiteType.NotSet);
+            //InitServicePlaylists();
+
+            SupportedStates = new List<StateImage>();
+            foreach (KeyValuePair<string, object> pair in supportedStates)
+            {
+                SupportedStates.Add(new StateImage(pair.Value, pair.Key));
+            }
+            SelectedState = SupportedStates.First();
         }
 
         #endregion
@@ -66,6 +82,8 @@ namespace Crawler.ViewModels
         #endregion
 
         #region Methods
+
+        public StateImage SelectedState { get; set; }
 
         public void AddToStateList(object state, IVideoItem item)
         {
@@ -298,5 +316,24 @@ namespace Crawler.ViewModels
         }
 
         #endregion
+
+        public List<StateImage> SupportedStates { get; private set; }
+
+        public class StateImage
+        {
+            public StateImage(object state, string resourcepic)
+            {
+                State = state;
+                Stream img = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourcepic);
+                if (img != null)
+                {
+                    Thumbnail = StreamHelper.ReadFully(img);
+                }
+            }
+
+            public object State { get; private set; }
+
+            public byte[] Thumbnail { get; private set; }
+        }
     }
 }
